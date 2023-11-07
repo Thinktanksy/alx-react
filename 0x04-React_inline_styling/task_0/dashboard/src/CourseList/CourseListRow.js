a92b50f7e713ad414e38e755d5c7511470c53f02
Joseph Williams <richlike.joseph@gmail.com>
2023-11-06 21:36:24 -0800

    Styling

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const headerStyle = {
-	backgroundColor: '#deb5b545',
-};
-
-const normalRowStyle = {
-	backgroundColor: '#f5f5f5ab',
-};
-
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
 	return (
-		<tr style={normalRowStyle}>
+		<tr>
 			{isHeader ? (
 				textSecondCell === null ? (
-					<th style={headerStyle} colSpan={2}>
-						{textFirstCell}
-					</th>
+					<th colSpan={2}>{textFirstCell}</th>
 				) : (
 					<>
-						<th style={headerStyle}>{textFirstCell}</th>
-						<th style={headerStyle}>{textSecondCell}</th>
+						<th>{textFirstCell}</th>
+						<th>{textSecondCell}</th>
 					</>
 				)
 			) : (
